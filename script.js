950a91b3591b3b9bb6e842e5596827743f553fff
Danielle <danielle@kendallspace.net>
2022-02-07 13:48:27 -0500

    added a scoreboard to index.html; implemented keeping score and declaring a winner in script.js; once a player wins, the game is reset

--- script.js
+++ script.js
@@ -1,61 +1,73 @@
 //script plays a game of rock, paper, scissors where a player plays the computer
 
-//write a function called game -> ALL LOGIC FOR THE GAME INSIDE THIS FUNCTION
-function game() {
-    //initialize global variables
-    let roundNumber = 1;
-    let playerWin = 0;
-    let computerWin = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-    //get player input by listening for button click
-    const choices = document.querySelectorAll(".choices");
+//get player input by listening for button click
+const choices = document.querySelectorAll(".choices");
 
-    choices.forEach((choice) => {
-        choice.addEventListener("click", function () {
-            playerSelection = this.textContent;
+choices.forEach((choice) => {
+    choice.addEventListener("click", function () {
+        playerSelection = this.textContent;
 
-            //generate computer input
-            const computerChoices = ["rock", "paper", "scissors"];
-            const computerSelection = computerChoices[Math.floor(Math.random() * 3)];
+        //generate computer input
+        const computerChoices = ["rock", "paper", "scissors"];
+        const computerSelection = computerChoices[Math.floor(Math.random() * 3)];
 
-            //call playRound
-            playRound(playerSelection, computerSelection);
-        });
+        //play the round with player and computer choices
+        playRound(playerSelection, computerSelection);
+
+        //update score in browser
+        updateScore();
+
+        //check for a winner, and reset game if there is one
+        if (checkWinner()) {
+            playerScore = computerScore = 0;
+            updateScore();
+        }
     });
+});
 
-    //write a function called playRound that plays a single round of Rock Paper Scissors
-    function playRound (playerSelection, computerSelection) {
-        //compare player vs computer selections to decide winner of current round
-        const currentRound = `You chose: ${playerSelection}\nComputer chose: ${computerSelection}`;
+//function that plays a single round of Rock Paper Scissors
+function playRound (playerSelection, computerSelection) {
+    //compare player vs computer selections to decide winner of current round
+    const currentRound = `You chose: ${playerSelection}\nComputer chose: ${computerSelection}`;
 
-        //if playerSelection equals computerSelection, the round is a tie
-        if (playerSelection === computerSelection) {
-            alert(`${currentRound}\nThis round is a tie!`);
-        }  
-        //if player wins round
-        if ((playerSelection === "rock" && computerSelection === "scissors") ||
-            (playerSelection === "paper" && computerSelection === "rock") ||
-            (playerSelection === "scissors" && computerSelection === "paper")) {
-            alert(`${currentRound}\nYou win!`)
-        }
-        //if player loses round
-        if ((playerSelection === "rock" && computerSelection === "paper") ||
-            (playerSelection === "paper" && computerSelection === "scissors") ||
-            (playerSelection === "scissors" && computerSelection === "rock")) {
-            alert(`${currentRound}\nYou lose!`);
-        }
+    //if the round is a tie
+    if (playerSelection === computerSelection) {
+        alert(`${currentRound}\nThis round is a tie!`);
+    }  
+    //if player wins
+    if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+        alert(`${currentRound}\nYou win!`)
+        playerScore++;
     }
-
-    //announce winner once either player or computer wins 5 rounds
-    if (playerWin === 5) {
-        //let player know they won
-        console.log("Congratulations! You've beaten the Computer!");
-        //return "Congratulations! You've beaten the Computer!";
-    } else if (computerWin === 5) {
-        //let player know they lost
-        console.log("Sad Panda! The Computer has bested you!");
-        //return "Sad Panda! The Computer has bested you!";
+    //if player loses
+    if ((playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "rock")) {
+        alert(`${currentRound}\nYou lose!`);
+        computerScore++;
     }
 }
-//call game function at the end
-game();
+
+//function that updates the score in the browser
+function updateScore() {
+    document.getElementById("playerScore").textContent = playerScore;
+    document.getElementById("computerScore").textContent = computerScore;
+}
+
+//function that checks for a winner once either player or computer wins 5 rounds
+function checkWinner() {
+    if (playerScore === 5) {
+        alert("Congratulations! You've beaten the Computer!");
+        return true;
+    } else if (computerScore === 5) {
+        alert("Sad Panda! The Computer has bested you!");
+        return true;
+    } else {
+        return false;
+    }
+}
