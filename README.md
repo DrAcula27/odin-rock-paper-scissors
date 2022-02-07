895edd8d1963ce3ba43d21e45b94593af453c2c8
Danielle <danielle@kendallspace.net>
2022-02-07 12:00:56 -0500

    updated README.md; added basic html elements (the rock paper scissors buttons and some text elements) to index.html; refactored script.js to remove logic that plays 5 rounds, added function to get player input via a button click event listener, moved computer choice generation and playRound call into player input function

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 <p>I will be building a simple implementation of the classic "rock-paper-scissors" game from scratch using JavaScript.</p>
 <p>The first iteration of this game will be played soley within the console. I will revisit this project later in the JavaScript Foundations to add a GUI using HTML and CSS.</p>
 
-<strong>After completing this project, I will have accomplished:</strong>
+<strong>After completing this project, I have accomplished:</strong>
 <ul>
     <li>Continued improvement of version control using Git and GitHub</li>
     <li>Writing javascript functions using detailed, yet concise, names</li>
@@ -16,3 +16,20 @@
     <li>Understanding the importance of commenting code</li>
 </ul>
 <strong>Project Source:</strong> https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors
+
+<h1>UPDATE 1</h1>
+<strong>The Odin Project - Foundations - JavaScript Basics - Revisiting Rock, Paper, Scissors</strong>
+
+<p>The goal is to add a User Interface (UI) to the game, so that it is no longer played via the console.</p>
+
+<strong>After completing this update, I will have accomplished:</strong>
+<ul>
+    <li>Learn how to create and manage branches in Git</li>
+    <li>Understand what the DOM (Document Object Model) is in relation to a webpage</li>
+    <li>Understand the difference between a node and an element</li>
+    <li>Learn how to target nodes with selectors</li>
+    <li>Learn the basic methods for finding/adding/removing and altering DOM nodes</li>
+    <li>Practice debugging code</li>
+    <li>Using event listeners to get inputs from a user</li>
+    <li>Refactoring code</li>
+</ul>
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//script runs a console-based game of rock, paper, scissors where a player plays the computer
+//script plays a game of rock, paper, scissors where a player plays the computer
 
 //write a function called game -> ALL LOGIC FOR THE GAME INSIDE THIS FUNCTION
 function game() {
@@ -7,85 +7,54 @@ function game() {
     let playerWin = 0;
     let computerWin = 0;
 
-    //write a function called computerPlay to generate computer's choice
-    function computerPlay () {
-        //generate a random number between 0 and 2 inclusive
-        let computerSelection = Math.floor(Math.random()*3);
-        //for 0, return 'rock'; for 1, return 'paper'; for 2, return 'scissors'
-        return computerSelection === 0 ? "rock" : computerSelection === 1 ? "paper" : "scissors";
-    }
+    //get player input by listening for button click
+    const choices = document.querySelectorAll(".choices");
 
-    //ask player for input of 'rock', 'paper', or 'scissors' (case insensitive)
-    function getPlayerChoice () {
-        const playerSelection = prompt("Choose your weapon: rock, paper, or scissors").toLowerCase();
-        //validate player input
-        if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
-            return playerSelection;
-        } else {
-            //if rock, paper, or scissors isn't chosen, tell player to correct their choice
-            console.log("please choose rock, paper, or scissors");
-        }
-    }
+    choices.forEach((choice) => {
+        choice.addEventListener("click", function () {
+            playerSelection = this.textContent;
 
-    //write a function called playRound that takes two parameters and
-    //plays a single round of Rock Paper Scissors
+            //generate computer input
+            const computerChoices = ["rock", "paper", "scissors"];
+            const computerSelection = computerChoices[Math.floor(Math.random() * 3)];
+
+            //call playRound
+            playRound(playerSelection, computerSelection);
+        });
+    });
+
+    //write a function called playRound that plays a single round of Rock Paper Scissors
     function playRound (playerSelection, computerSelection) {
         //compare player vs computer selections to decide winner of current round
+        const currentRound = `You chose: ${playerSelection}\nComputer chose: ${computerSelection}`;
+
         //if playerSelection equals computerSelection, the round is a tie
-        if ((playerSelection === "rock" && computerSelection === "rock") ||
-            (playerSelection === "paper" && computerSelection === "paper") ||
-            (playerSelection === "scissors" && computerSelection === "scissors")) {
-            //move onto next round, let player know it's a tie
-            console.log(`Round: ${roundNumber++};
-                        Player Chose: ${playerSelection} - Player Score: ${playerWin};
-                        Computer Chose: ${computerSelection} - Computer Score: ${computerWin};
-                        It's a tie!`);
-            //return (roundNumber, playerWin, result = "It's a tie!");
+        if (playerSelection === computerSelection) {
+            alert(`${currentRound}\nThis round is a tie!`);
         }  
         //if player wins round
         if ((playerSelection === "rock" && computerSelection === "scissors") ||
             (playerSelection === "paper" && computerSelection === "rock") ||
             (playerSelection === "scissors" && computerSelection === "paper")) {
-            //move onto next round, add to player score, let player know they won
-            console.log(`Round: ${roundNumber++};
-                        Player Chose: ${playerSelection} - Player Score: ${++playerWin};
-                        Computer Chose: ${computerSelection} - Computer Score: ${computerWin};
-                        You win!`);
-            //return (roundNumber++, ++playerWin, result = "You win!");
+            alert(`${currentRound}\nYou win!`)
         }
         //if player loses round
         if ((playerSelection === "rock" && computerSelection === "paper") ||
             (playerSelection === "paper" && computerSelection === "scissors") ||
             (playerSelection === "scissors" && computerSelection === "rock")) {
-            //move onto next round, add to computer score, let player know they lost
-            console.log(`Round: ${roundNumber++};
-                        Player Chose: ${playerSelection} - Player Score: ${playerWin};
-                        Computer Chose: ${computerSelection} - Computer Score: ${++computerWin};
-                        You lose!`);
-            //return (roundNumber++, ++computerWin, result = "You lose!");
+            alert(`${currentRound}\nYou lose!`);
         }
     }
 
-    //run playRound 5 times (loop this later)
-    playRound(getPlayerChoice(), computerPlay()); //round 1
-    playRound(getPlayerChoice(), computerPlay()); //round 2
-    playRound(getPlayerChoice(), computerPlay()); //round 3
-    playRound(getPlayerChoice(), computerPlay()); //round 4
-    playRound(getPlayerChoice(), computerPlay()); //round 5
-
-    //announce winner at the end of 5 rounds (for a best of 5)
-    if (playerWin > computerWin) {
+    //announce winner once either player or computer wins 5 rounds
+    if (playerWin === 5) {
         //let player know they won
         console.log("Congratulations! You've beaten the Computer!");
         //return "Congratulations! You've beaten the Computer!";
-    } else if (computerWin > playerWin) {
+    } else if (computerWin === 5) {
         //let player know they lost
         console.log("Sad Panda! The Computer has bested you!");
         //return "Sad Panda! The Computer has bested you!";
-    } else {
-        //let player know it's a cats game
-        console.log("After 5 rounds, nobody wins...");
-        //return "After 5 rounds, nobody wins...";
     }
 }
 //call game function at the end
